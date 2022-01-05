c7d4f279c986df68f756f59c5790043c8c36a159
Zan Lwine Htoo <zanlwine.htoo110@gmail.com>
2022-01-05 15:57:38 +0630

    Frontend UI

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -14,7 +14,7 @@
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     {{-- font awesome --}}
-    <link rel="stylesheet" href="{{asset('fontawesome/css/all.css')}}">
+    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.css') }}">
 
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
     @yield('extra_css')
@@ -23,71 +23,53 @@
 
 <body>
     <div id="app">
-        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+        <div class="header-menu">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="row">
+                        <div class="col-4 text-center">
+                        </div>
+                        <div class="col-4 text-center">
+                            <a href="" class="">
+                                <h3 >Magic Pay</h3>
+                            </a>
+                        </div>
+                        <div class="col-4 text-center">
+                            <a href="" class="">
+                                <i class="fas fa-bell"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </nav> --}}
+        </div>
 
-        <main class="py-4">
+        {{-- <main class="py-4">
             @yield('content')
-        </main>
+        </main> --}}
 
         <div class="bottom-menu">
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="row">
                         <div class="col-4 text-center">
-                            <i class="fas fa-home"></i>
-                            <p class="mb-0">Home</p>
+                            <a href="">
+                                <i class="fas fa-home"></i>
+                                <p class="mb-0">Home</p>
+                            </a>
                         </div>
-                        <div class="col-4 text-center">Scan</div>
-                        <div class="col-4 text-center">Account</div>
+                        <div class="col-4 text-center">
+                            <a href="" class="">
+                                <i class="fas fa-qrcode"></i>
+                                <p class="mb-0">Scan</p>
+                            </a>
+                        </div>
+                        <div class="col-4 text-center">
+                            <a href="" class="">
+                                <i class="fas fa-user"></i>
+                                <p class="mb-0">Account</p>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
