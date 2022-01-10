29599e846b4b0d3b2d95e04a038aeede1778b505
Zan Lwine Htoo <zanlwine.htoo110@gmail.com>
2022-01-10 10:13:02 +0630

    Transfer & Transaction db

--- app/Helpers/UUIDGenerate.php
+++ app/Helpers/UUIDGenerate.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Helpers;
+
+use App\Transaction;
 use App\Wallet;
 
 class UUIDGenerate{
@@ -8,6 +10,22 @@ class UUIDGenerate{
         $number = mt_rand(1000000000000000,9999999999999999);
         if(Wallet::where('account_number',$number)->exists()){
             self::accountNumber();
+        }
+        return $number;
+    }
+
+    public static function refNumber(){
+        $number = mt_rand(1000000000000000,9999999999999999);
+        if(Transaction::where('ref_no',$number)->exists()){
+            self::refNumber();
+        }
+        return $number;
+    }
+
+    public static function trxId(){
+        $number = mt_rand(1000000000000000,9999999999999999);
+        if(Transaction::where('trx_id',$number)->exists()){
+            self::trxId();
         }
         return $number;
     }
--- app/Http/Controllers/Frontend/PageController.php
+++ app/Http/Controllers/Frontend/PageController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Helpers\UUIDGenerate;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TransferFormValidate;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\UpdatePassword;
+use App\Transaction;
 use App\User;
+use Illuminate\Support\Facades\DB;
 
 class PageController extends Controller
 {
@@ -59,6 +62,10 @@ class PageController extends Controller
             return back()->withErrors(['amount' =>'The amount must be at least 1000 MMK'])->withInput();
         }
 
+        if($request->amount > $user->wallet->amount){
+            return back()->withErrors(['amount' =>'Insufficient Balance. Please check your wallet.'])->withInput();
+        }
+
         if($user->phone == $request->to_phone){
             return back()->withErrors(['to_phone'=>'To account is invalid'])->withInput();
         }
@@ -97,15 +104,47 @@ class PageController extends Controller
             return back()->withErrors(['fail'=>'Something went wrong! The given data is invalid.'])->withInput();
         }
 
-        $from_account_wallet = $from_account->wallet;
-        $from_account_wallet->decrement('amount',$amount);
-        $from_account_wallet->update();
+        DB::beginTransaction();
+        try {
+            $from_account_wallet = $from_account->wallet;
+            $from_account_wallet->decrement('amount',$amount);
+            $from_account_wallet->update();
 
-        $to_account_wallet = $to_account->wallet;
-        $to_account_wallet->increment('amount',$amount);
-        $to_account_wallet->update();
+            $to_account_wallet = $to_account->wallet;
+            $to_account_wallet->increment('amount',$amount);
+            $to_account_wallet->update();
 
-        return redirect('/')->with('transfer-success', 'Successfully transferred.');
+            $ref_no = UUIDGenerate::refNumber();
+
+            $from_account_transaction = new Transaction();
+            $from_account_transaction->ref_no = $ref_no;
+            $from_account_transaction->trx_id = UUIDGenerate::trxId();
+            $from_account_transaction->user_id = $from_account->id ;
+            $from_account_transaction->type = 2;
+            $from_account_transaction->amount = $amount ;
+            $from_account_transaction->source_id = $to_account->id ;
+            $from_account_transaction->description = $description;
+            $from_account_transaction->save();
+
+            $to_account_transaction = new Transaction();
+            $to_account_transaction->ref_no = $ref_no;
+            $to_account_transaction->trx_id = UUIDGenerate::trxId();
+            $to_account_transaction->user_id = $to_account->id ;
+            $to_account_transaction->type =1 ;
+            $to_account_transaction->amount = $amount;
+            $to_account_transaction->source_id =$from_account->id ;
+            $to_account_transaction->description =$description ;
+            $to_account_transaction->save();
+
+            
+
+            DB::commit();
+            return redirect('/')->with('transfer-success', 'Successfully transferred.');
+
+        } catch (\Exception $error) {
+            DB::rollBack();
+            return back()->withErrors(['fail' =>'Something Wrong' . $error->getMessage()])->withInput();
+        }
     }
 
     public function toAccountVerify(Request $request){
